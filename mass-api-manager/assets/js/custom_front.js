ff7bc818565579b105116b7cdb7818185e5c9640
webdeveloper-eng <53304877+webdeveloper-eng@users.noreply.github.com>
2019-09-01 23:26:41 +0700

    Add files via upload

--- mass-api-manager/assets/js/custom_front.js
+++ mass-api-manager/assets/js/custom_front.js
@@ -1,7 +1,27 @@
 jQuery(document).ready(function(){
-    jQuery('.mam_network_item a').mouseover(function(){
-        jQuery('.mam_campaign_main_container a').attr('href', jQuery(this).attr('href'));
-        jQuery('.mam_campaign_main_container a img').attr('src', jQuery(jQuery(this).find('img')).attr('src'));
-        jQuery('.mam_campaign_main_container a:last-child span').text(jQuery(jQuery(this).next()).val());
+    var width = jQuery('.mam_campaign_main_container').width();
+    jQuery('.mam_campaign_main_container').height(width / 4 * 3);
+    jQuery('.show_caption_area').height('700px');
+
+    jQuery('.mam_network_item').click(function(){
+        jQuery('.mam_campaign_main_container').attr('src', jQuery(this).find('input').val());
+    });
+
+    jQuery(window).resize(function(){
+        var width = jQuery('.mam_campaign_main_container').width();
+        jQuery('.mam_campaign_main_container').height(width / 4 * 3);
+    });
+
+    jQuery('.btn_get_caption').click(function(){
+        jQuery.ajax({
+            url: './wp-content/plugins/mass-api-manager/youtube_caption_scrapper.php',
+            data: { 'kind': 'show_caption', 'videoid': jQuery('.txt_video_id').val() },
+            type: 'post',
+            success: function (result) {
+                jQuery('.show_caption_area').val(result)
+            }
+        });
+
     });
 });
+
